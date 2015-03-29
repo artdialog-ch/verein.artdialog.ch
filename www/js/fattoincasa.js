401e53945d5c9e805d8353c0efedfc5b3c1a1750
roman-mkh <ramses_ru@outlook.com>
2015-03-30 00:02:24 +0200

    reintegrated changes from Nicole's developers

--- www/js/fattoincasa.js
+++ www/js/fattoincasa.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
-	var adnav = $("#ad-nav");
-	adnav.affix({
-        offset: { 
+    var adnav = $("#ad-nav");
+    adnav.affix({
+        offset: {
             top: adnav.offset().top
         }
     });
-	
-	new WOW().init({ mobile: false});
-	
-	if (window.location.search.substring(1).indexOf('debug') >=0)
-	{
-		$(".debug").removeClass("debug");
-	}
+    
+    new WOW().init({
+        mobile: false
+    });
+
+    if (window.location.search.substring(1).indexOf('debug') >= 0) {
+        $(".debug").removeClass("debug");
+    }
 });
 
 $(document).ready($(function() {
     $(".ad-panini img")
-        .mouseover(function() { 
+        .mouseover(function() {
             var src = $(this).attr("src").replace(/_bw/, '_cl');
             $(this).attr("src", src);
         })
@@ -31,7 +32,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - $('.affix').height()
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -40,4 +41,14 @@ $(function() {
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
+});
+
+//
+
+$('nav').on('affix.bs.affix', function (){
+     $('body').css('margin-top', $('nav').height());
+});
+
+$('nav').on('affix-top.bs.affix', function (){
+     $('body').css('margin-top', - 98);
 });
